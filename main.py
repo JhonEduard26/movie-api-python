f98e137fbb6c61ad1d2218c3ec51b991ba728d0e
jhon <jhon.bocanegra26@gmail.com>
2023-07-15 16:44:21 -0500

    add: body params

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from uuid import uuid4
+from pydantic import BaseModel
+
+
+class Movie(BaseModel):
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
+
 
 app = FastAPI()
 app.title = 'FastAPI movie API'
+
 
 movies = [
     {
@@ -20,6 +31,14 @@ movies = [
         'year': '2004',
         'rating': 8,
         'category': 'Thriller'
+    },
+    {
+        'id': uuid4(),
+        'title': 'Scream 2',
+        'overview': 'lorem lorem lorem',
+        'year': '2007',
+        'rating': 7,
+        'category': 'Thriller'
     }
 ]
 
@@ -36,8 +55,27 @@ async def get_movies():
 
 @app.get('/movies/{id_movie}', tags=['movies'])
 async def get_movie_by_id(id_movie: str):
-    movie = [item for item in movies if str(item['id']) == id_movie]
+    movie = list(filter(lambda item: str(item['id']) == id_movie, movies))
     return {
         'ok': True,
         'movie': movie
     }
+
+
+@app.get('/movies/', tags=['movies'])
+async def get_movies_by_category(category: str):
+    movies_filtered = list(filter(lambda item: item['category'] == category, movies))
+    return {
+        'ok': True,
+        'movies': movies_filtered,
+    }
+
+
+@app.post('/movies', tags=['movies'])
+async def create_movie(movie: Movie):
+    new_movie = {
+        'id': uuid4(),
+        **movie.model_dump()
+    }
+    movies.append(new_movie)
+    return new_movie
